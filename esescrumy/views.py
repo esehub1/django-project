9708a215da6affd62ed72ace058bf0091fcd254d
esehub1 <luvese2009@gmail.com>
2022-06-20 14:16:19 -0400

    this django project and app

---
+++ esescrumy/views.py
@@ -0,0 +1,7 @@
+'from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+def get_grading_parameters(request):
+    return HttpResponse("Welcome to Django")
